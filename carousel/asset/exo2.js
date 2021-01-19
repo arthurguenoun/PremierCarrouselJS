bad466208981b18d0e8112db2910ea28f609d63f
arthurguenoun <63154005+arthurguenoun@users.noreply.github.com>
2021-01-19 22:54:48 +0100

    Add files via upload

---
+++ carousel/asset/exo2.js
@@ -0,0 +1,111 @@
+'use strict';
+	document.getElementById('avengersMini').addEventListener('click', function(){
+	document.querySelector('#grosse').src = "sideshow/avengers.jpg";
+	document.querySelector('figcaption').innerHTML="Les Avengers !";
+	document.querySelector('#grosse').alt= "l'équipe des Avengers";
+	var selectAll = document.querySelectorAll('#photoMini img');
+		 for( let j = 0; j < selectAll.length; j++){
+            selectAll[j].style.border ="3px solid black";
+        }
+	document.getElementById('avengersMini').style.border="3px solid red";
+	
+});
+
+document.getElementById('blacksadMini').addEventListener('click', function(){
+	document.querySelector('#grosse').src = "sideshow/blacksad.jpg"
+	document.querySelector('figcaption').innerHTML="BlackSad !";
+	document.querySelector('#grosse').alt= "c'est BlackSad!";
+		var selectAll = document.querySelectorAll('#photoMini img');
+		 for(let j = 0; j < selectAll.length; j++){
+            selectAll[j].style.border ="3px solid black";
+        }
+	document.getElementById('blacksadMini').style.border="3px solid red";
+});
+
+document.getElementById('comancheMini').addEventListener('click', function(){
+	document.querySelector('#grosse').src = "sideshow/comanche.jpg"
+	document.querySelector('figcaption').innerHTML="Comanche !";
+	document.querySelector('#grosse').alt= "c'est Comanche!";
+	var selectAll = document.querySelectorAll('#photoMini img');
+		 for(let j = 0; j < selectAll.length; j++){
+            selectAll[j].style.border ="3px solid black";
+        }
+	document.getElementById('comancheMini').style.border="3px solid red";
+});
+
+document.getElementById('decapeMini').addEventListener('click', function(){
+	document.querySelector('#grosse').src = "sideshow/decapeetdecrocs.jpg"
+	document.querySelector('figcaption').innerHTML="Decape et decrocs !";
+	document.querySelector('#grosse').alt= "c'est Decape et Decrocs!";
+	var selectAll = document.querySelectorAll('#photoMini img');
+		 for(let j = 0; j < selectAll.length; j++){
+            selectAll[j].style.border ="3px solid black";
+        }
+	document.getElementById('decapeMini').style.border="3px solid red";
+});
+
+document.getElementById('jeremiahMini').addEventListener('click', function(){
+	document.querySelector('#grosse').src = "sideshow/jeremiah.jpg"
+	document.querySelector('figcaption').innerHTML="Jeremiah !";
+	document.querySelector('#grosse').alt= "c'est Jeremiah !";
+	var selectAll = document.querySelectorAll('#photoMini img');
+		 for(let j = 0; j < selectAll.length; j++){
+            selectAll[j].style.border ="3px solid black";
+        }
+	document.getElementById('jeremiahMini').style.border="3px solid red";
+});
+
+document.getElementById('saiyukiMini').addEventListener('click', function(){
+	document.querySelector('#grosse').src = "sideshow/saiyuki.jpg"
+	document.querySelector('figcaption').innerHTML="Saiyuki !";
+	document.querySelector('#grosse').alt= "c'est Saiyuki!";
+	var selectAll = document.querySelectorAll('#photoMini img');
+		 for(let j = 0; j < selectAll.length; j++){
+            selectAll[j].style.border ="3px solid black";
+        }
+	document.getElementById('saiyukiMini').style.border="3px solid red";
+});
+
+document.getElementById('SantiagMini').addEventListener('click', function(){
+	document.querySelector('#grosse').src = "sideshow/santiag.jpg"
+	document.querySelector('figcaption').innerHTML="Santiag !";
+	document.querySelector('#grosse').alt= "c'est Santiag!";
+	var selectAll = document.querySelectorAll('#photoMini img');
+		 for(let j = 0; j < selectAll.length; j++){
+            selectAll[j].style.border ="3px solid black";
+        }
+	document.getElementById('SantiagMini').style.border="3px solid red";
+});
+
+document.getElementById('spirouMini').addEventListener('click', function(){
+	document.querySelector('#grosse').src = "sideshow/spirou.jpg"
+	document.querySelector('figcaption').innerHTML="Spirou !";
+	document.querySelector('#grosse').alt= "c'est Spirou!";
+	var selectAll = document.querySelectorAll('#photoMini img');
+		 for(let j = 0; j < selectAll.length; j++){
+            selectAll[j].style.border ="3px solid black";
+        }
+	document.getElementById('spirouMini').style.border="3px solid red";
+});
+
+document.getElementById('trollsMini').addEventListener('click', function(){
+	document.querySelector('#grosse').src = "sideshow/trollsdetroy.jpg"
+	document.querySelector('figcaption').innerHTML="Trolls Detroys !";
+	document.querySelector('#grosse').alt= "c'est Trolls Detroy!";
+	var selectAll = document.querySelectorAll('#photoMini img');
+		 for(let j = 0; j < selectAll.length; j++){
+            selectAll[j].style.border ="3px solid black";
+        }
+	document.getElementById('trollsMini').style.border="3px solid red";
+});
+
+document.getElementById('xiiiMini').addEventListener('click', function(){
+	document.querySelector('#grosse').src = "sideshow/xiii.jpg"
+	document.querySelector('figcaption').innerHTML="XIII !";
+	document.querySelector('#grosse').alt= "c'est XIII !";
+	var selectAll = document.querySelectorAll('#photoMini img');
+		 for(let j = 0; j < selectAll.length; j++){
+            selectAll[j].style.border ="3px solid black";
+        }
+	document.getElementById('xiiiMini').style.border="3px solid red";
+});
